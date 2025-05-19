3d71bf7d5a814b74f1bd261d967bc58efda99dee
Sebastian Heckers <sebastian.heckers@inf.ethz.ch>
2025-05-19 17:40:46 +0200

    adapted new run scripts

--- run.sh
+++ run.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+#
+#SBATCH --job-name=DH6-walk-test
+#
 #SBATCH --gpus=1
 #SBATCH --account digital_human
-#SBATCH --time=48:00:00
-#SBATCH --output=nvidia-smi.out
-source ./demos/scene.sh
-python -m visualize.vis_seq --add_floor 0 --seq_path './mld_denoiser/mld_fps_clip_repeat_euler/checkpoint_300000/optim/sit_use_pred_joints_ddim10_guidance5.0_seed0_contact0.1_thresh0.0_collision0.1_jerk0.1/sample_*.pkl'
+#SBATCH --time=01:00:00
+#SBATCH --output=walk-test.out
+
+conda activate DART
+
+# RUN OPTIM
+source ./demos/leglos.sh
+
+# VISUALIZE
+# python -m visualize.vis_seq --add_floor 0 --seq_path './mld_denoiser/mld_fps_clip_repeat_euler/checkpoint_300000/optim/sit_use_pred_joints_ddim10_guidance5.0_seed0_contact0.1_thresh0.0_collision0.1_jerk0.1/sample_*.pkl'
 
