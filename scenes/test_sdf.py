07f2ec9f1bdd3690aaad0b9f30703e5f3628b017
Sebastian Heckers <sebastian.heckers@inf.ethz.ch>
2025-06-03 15:21:49 +0200

    new scripts for scene files

--- scenes/test_sdf.py
+++ scenes/test_sdf.py
@@ -13,8 +13,16 @@ def test_mesh2sdf(filename, size=128, level=1/128, mesh_scale = 0.8):
     # level = 2 / size  # recommended level = 2 / size
     mesh = trimesh.load(filename, force='mesh')
 
-    # normalize mesh
+    # set floor
     vertices = mesh.vertices
+    floor = np.min(vertices[:,1])
+    vertices = vertices + np.array([0,-floor,0])
+
+    # change axis orientation
+    vertices[:, [0,1,2]] = vertices[:, [2,0,1]]
+    vertices[:, 0] = -vertices[:, 0]
+
+    # normalize mesh
     bbmin = vertices.min(0)
     bbmax = vertices.max(0)
     center = (bbmin + bbmax) * 0.5
@@ -41,8 +49,14 @@ def test_mesh2sdf(filename, size=128, level=1/128, mesh_scale = 0.8):
     # print('It takes %.4f seconds to process %s' % (t1-t0, filename))
 
 
-scene_dir_list = [Path('/home/kaizhao/projects/multiskill/data/scenes/demo/62b1714ef66f4e0d9f42dcd12efb3f52/')]
+scene_dir_list = [Path('/home/sebastian/GIT/DH/iDART/data/scenes/cab_e'),
+                  Path('/home/sebastian/GIT/DH/iDART/data/scenes/cab_g_benches'),
+                  Path('/home/sebastian/GIT/DH/iDART/data/scenes/foodlab_0312'),
+                  Path('/home/sebastian/GIT/DH/iDART/data/scenes/cnb_dlab_0215'),
+                  Path('/home/sebastian/GIT/DH/iDART/data/scenes/seminar_h53_0218')]
+# scene_dir_list = [Path('/home/kaizhao/projects/multiskill/data/scenes/demo/62b1714ef66f4e0d9f42dcd12efb3f52/')]
 size = 256
 for scene_dir in tqdm(scene_dir_list):
-    filename = str(scene_dir / 'scene.obj')
+    # filename = str(scene_dir / 'scene.obj')
+    filename = str(scene_dir) + '/' + str(str(scene_dir).split('/')[-1] + '.obj')
     test_mesh2sdf(filename, size=size, level=1/size, mesh_scale=0.8)
